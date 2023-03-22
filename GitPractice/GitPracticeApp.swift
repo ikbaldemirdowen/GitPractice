8ea54ccd5535f02cba9a4d1948e0f1ee0c0c17e5
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-03-21 23:31:07 -0700

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Ikbal Demirdoven on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
